09c09f5ab058ad872b651d501ed22e25368e16c6
Pulkit Gupta <pulkitgupta045@gmail.com>
2023-01-27 23:56:57 +0530

    created student course model

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express, { json } from 'express';
-import cors from 'cors';
-import db from './config/mongoose.js';
+import express, { json } from "express";
+import cors from "cors";
+import db from "./config/mongoose.js";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -10,9 +10,6 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-
-
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
