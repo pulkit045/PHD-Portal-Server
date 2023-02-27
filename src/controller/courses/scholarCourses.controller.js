4a11950c8d8ae1bfd38c0a766b357de878904791
Pulkit Gupta <pulkitgupta045@gmail.com>
2023-02-27 15:05:58 +0530

    Auto updating the facukties corresponding courses and sending required faculty only

--- src/controller/courses/scholarCourses.controller.js
+++ src/controller/courses/scholarCourses.controller.js
@@ -1,8 +1,23 @@
 const Scholar = require("../../database/models/scholar.model");
 const Courses = require("../../database/models/courses.model");
-
+const Faculty = require("../../database/models/faculty.model");
 module.exports = async (req, res, next) => {
   const { course_id } = req.body;
+  try{
+    // this is for automatically updating the course filed in the faculty 
+    //if it does not exist and student has filled it
+    const facultyId = await Courses.find({_id : course_id} , ["faculty_id"]);
+    const courseId = await Faculty.find({_id : facultyId[0].faculty_id , course : {"$eq" : course_id}});
+    if(courseId.length == 0 && courseId != null){
+      await Faculty.updateOne({_id : facultyId[0].faculty_id},{
+        "$push" : {"course" : course_id}
+      });
+    }
+    // console.log(facultyId , courseId);
+  }catch(error){
+    console.log("error in updating the faculty id ");
+    next(error);
+  }
   try {
     await Scholar.updateOne(
       { _id: req.user.user._id },
--- src/controller/getAllFaculty.controller.js
+++ src/controller/getAllFaculty.controller.js
@@ -1,10 +1,21 @@
 const Faculty = require("../database/models/faculty.model");
-
+const Request = require("../database/models/requests.model");
 module.exports = async (req, res, next) => {
 	try {
 		const faculties = await Faculty.find({}, ["fullName", "_id"]);
+		const data = [];
+		for(let faculty of faculties){
+			// console.log(faculty);
+			const isRequested = await Request.find({scholar_id : req.user.user._id , supervisor_id : faculty._id});
+			// console.log(isRequested);
+			if(isRequested != null && isRequested.length == 0){
+				// console.log("Empty goes here");
+				// console.log(faculty);
+				data.push(faculty);
+			}
+		}
 		// console.log(faculties);
-		res.send(faculties);
+		res.send(data);
 	} catch (err) {
 		console.error("Error in get all faculty", err);
 		next(err);
